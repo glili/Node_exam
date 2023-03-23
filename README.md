7dc981e55289668b73fca1f8345ebda7bca9cf6d
Lili <you@example.com>
2023-03-22 22:02:50 -0400

    remove docker

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # node_exam
 Preparing for Nodejs certification
+
+
+    // "start": "rm -rf build/ && tsc -p src/ && node build/server.js"
--- beggining/src/server.ts
+++ beggining/src/server.ts
@@ -1,2 +1,99 @@
-let message: string = 'Hello World  blavla';
-console.log(message);
+// var first : number = 123;
+// console.log(first);
+
+// let fruits : string[] = ['Apple', 'Orange', 'Banana']; //or let fruits : Array<string> = ['Apple', 'Orange', 'Banana'];
+
+// console.log(fruits);
+
+// enum PrintMedia{
+//     Newspaper = "NEWSPAPER",
+//     Newsletter = "NEWSLETTER",
+//     Magazine = "MAGAZINE",
+//     Book = "BOOK"
+// }
+
+//   console.log(PrintMedia.Newspaper);
+// console.log(PrintMedia['Newsletter']);
+
+// console.log("Bonjour tout le monde");
+
+
+// const compteur = document.querySelector("#compteur");
+// let i =0;
+
+// const increment = (e : Event) =>{
+//     i++;
+//     compteur.querySelector("span").innerText = i.toString();
+// }
+
+// compteur.addEventListener('click', increment);
+
+
+//Ex1
+
+// namespace Exercice1{
+//     function addition(x:number, y: number){
+//         return x+y;
+//     }
+
+//     function sustraction(x:number, y: number){
+//         return x-y;
+//     }
+
+//     function multiplication(x:number, y: number){
+//         return x*y;
+//     }
+
+
+//     function division(x:number, y: number){
+//         return x/y;
+//     }
+
+//     console.log(addition(5, 8));
+//     console.log(sustraction(8,5));
+//     console.log(multiplication(5, 8));
+//     console.log(division(40,8));
+// }
+
+//Ex2
+
+// function addition(x:number, y: number){
+//             return x+y;
+//         }
+    
+//         function sustraction(x:number, y: number){
+//             return x-y;
+//         }
+    
+//         function multiplication(x:number, y: number){
+//             return x*y;
+//         }
+    
+    
+//         function division(x:number, y: number){
+//             return x/y;
+//         }
+    
+
+// Ex3
+
+var numbers : number[] = new Array(1,2,3,4,5,6);
+var numbersEven : number[];
+
+function getEven(arr : number[], arr2 : number[]){
+    for(var i=0; i<arr.length; i++){
+        if(numbers[i]%2==0){
+            numbersEven.push(numbers[i]);
+        }
+    }
+    console.log(numbersEven);
+}
+
+getEven(numbers, numbersEven);
+
+
+// Ex4
+
+// var years : number[] = new Array(1984, 2005, 2010, 2020);
+
+// function get
